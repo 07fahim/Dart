615dbcada530f4246ecfba5957dab875d42b2446
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-09-29 21:32:43 +0600

    added

---
+++ Simple_Bank_Management/gotham_bank.dart
@@ -0,0 +1,37 @@
+class BankAccount{
+
+  String accountName;
+  int accountId;
+  double _balance=0;
+
+  BankAccount({required this.accountName,required this.accountId});
+
+  double get balance => _balance;
+
+  void deposit(double amount){
+    if(amount>0){
+      _balance +=amount;
+      print("Deposit: ${amount.toStringAsFixed(3)}tk).New Balance: ${_balance.toStringAsFixed(3)}tk");
+    }else{
+      print('Invalid deposit amount');
+    }
+  }
+
+  void withdraw(double amount){
+    if(amount >0 && amount <=_balance){
+      _balance-=amount;
+      print("Withdraw : ${amount.toStringAsFixed(3)}tk. New Balance : ${_balance.toStringAsFixed(3)}tk");
+    }else if(amount>_balance){
+      print('Insufficient Fund');
+    }else{
+      print("Invalid withdraw amount");
+    }
+  }
+
+  void displayInfo(){
+    print('Account Name : $accountName');
+    print('Account Id : $accountId');
+    print('Current Balance : ${_balance.toStringAsFixed(3)}tk');
+  }
+}
+
